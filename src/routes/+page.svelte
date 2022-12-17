63aa967550ff6af7079bb56ec53144c1311c728c
Jeffrey Biles <bilesjeffrey@gmail.com>
2022-12-17 13:23:14 -0600

    Arrays and objects

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,48 @@
-<h1>Welcome to SvelteKit!!!</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+const products = [
+    {
+      id: 12,
+      name: 'Zynga Credits',
+      price: 29,
+    },
+    {
+      id: 92,
+      name: '1-way plane ticket to Las Vegas',
+      price: 174,
+    },
+    {
+      id: 73,
+      name: 'Roundup Weedkiller',
+      price: 12,
+    },
+    {
+      id: 66,
+      name: 'Seed Oils',
+      price: 4
+    },
+    {
+      id: 69,
+      name: "Oxycontin",
+      price: 87,
+    },
+    {
+      id: 666,
+      name: 'H****r Memorabilia',
+      price: 62,
+    },
+    {
+      id: 1,
+      name: 'Nuclear Warhead (3-bomb value pack!)',
+      price: 873_249
+    },
+  ]
+</script>
+
+<h1>Welcome to E(vil)-Commerce</h1>
+{#each products as product}
+  <h2>{product.name}</h2>
+  <p>${product.price}</p>
+{/each}
+<style>
+
+</style>
