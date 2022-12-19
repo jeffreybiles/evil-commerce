800b2c7d56730af990b28587943068b9c2d55785
Jeffrey Biles <bilesjeffrey@gmail.com>
2022-12-19 08:16:55 -0600

    Move data up one level, create item route with id

---
+++ src/routes/item/[item_id]/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { products, type Product } from "$lib/data";
+  import { page } from '$app/stores'
+  let product = products.find(p => p.id == $page.params.item_id) as Product
+
+</script>
+
+{product.id}
