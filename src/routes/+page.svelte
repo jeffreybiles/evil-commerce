d34985f26be347ab2f8211222caf9a12e8079446
Jeffrey Biles <bilesjeffrey@gmail.com>
2022-12-19 18:28:59 -0600

    Link to lessons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -28,11 +28,26 @@ const lessons: Lesson[] = [
 
 <h1>Welcome to E(vil)-Commerce</h1>
 {#each lessons as lesson}
-  <h2>{lesson.name}</h2>
+  <div class="lesson">
+    <h2>{lesson.name}</h2>
+    <a href="https://{lesson.exampleSubdomain}.evil-commerce.com" target="_blank" rel="noreferrer">
+      See it in action
+    </a>
+    {#each lesson.implementations as implementation}
+      <h3>{implementation.framework}</h3>
+      <a href={implementation.githubUrl} target="_blank" rel="noreferrer">See the code</a>
+      <a href={implementation.youtubeUrl} target="_blank">See the video</a>
+    {/each}
+  </div>
   <!-- TODO - link to the subdomain -->
   <!-- TODO - put framework icons for each of the implementations that are available, maybe direct links to the github repo and videos -->
   <!-- TODO - if comingSoon is true, then style it differently -->
 {/each}
 <style>
-
+.lesson {
+  border: 1px solid black;
+  padding: 1rem;
+  padding-top: 0;
+  margin: 1rem;
+}
 </style>
